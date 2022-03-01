33f845d52c610be5623b76eb4240705881343ca3
Laura Trentino <laura@trentino.com.au>
2022-03-01 17:44:17 +0800

    finalised requirements.txt

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template 
 import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy.engine import URL
+# from sqlalchemy import create_engine
+# from sqlalchemy.engine import URL
 import os 
 
 
